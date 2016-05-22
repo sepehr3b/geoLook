b9004ac5b000c4c9dfc9b651f9f600f10e77b928
Sepehr Behnami <sepehr.3b@gmail.com>
2016-05-22 17:39:21 +0430

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
-
+        alert(pos.lat);
         var newmap = L.map('mapid').setView([pos.lat, pos.lng], 16);
 
 
